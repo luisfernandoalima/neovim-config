bb90a67d44bfe6d378b3ee6e4e11ab7fa69e88cf
Luis Lima <luisfernando.lima2005@gmail.com>
2025-07-25 16:52:16 -0300

    Updating configs

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_enable = false,
+        ensure_installed = { "lua_ls" },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup {}
+    end
+  }
+}
